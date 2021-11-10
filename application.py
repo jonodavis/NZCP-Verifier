dc7e95d8fec39e8cecd3f66ab15a04f40d2ac635
Jono Davis <jono.davis66@gmail.com>
2021-11-11 03:57:47 +1300

    fixed CORS issues

--- application.py
+++ application.py
@@ -1,18 +1,18 @@
 import verifier
 from flask import Flask, app, request, jsonify
+from flask_cors import CORS
 
 application = Flask(__name__)
+CORS(application)
 
 @application.route('/verify', methods=['POST'])
 def verify():
   try:
     req = request.get_json()
     res = jsonify(verifier.check_code(req["payload"]))
-    res.headers.add('Access-Control-Allow-Origin', '*')
     return res
   except:
     res = jsonify({"validated": False})
-    res.headers.add('Access-Control-Allow-Origin', '*')
     return {"validated": False} 
 
 if __name__ == '__main__':
